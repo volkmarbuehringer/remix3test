b4efdc6f2e9f3ca446f373cb20af2045187c00ad
volkmarbuehringer <vobuba@freenet.de>
2026-06-14 13:44:29 +0200

    remove gaga test scaffold

--- gaga/app/assets/entry.ts
+++
@@ -1,8 +0,0 @@
-import { run } from 'remix/ui'
-
-run({
-  async loadModule(moduleUrl, exportName) {
-    let mod = await import(moduleUrl)
-    return mod[exportName]
-  },
-})
--- gaga/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { get, route } from 'remix/routes'
-
-export const routes = route({
-  assets: get('/assets/*path'),
-  home: '/',
-})
